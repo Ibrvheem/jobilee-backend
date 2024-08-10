196a6e6a34e5e9d617a584d814629ac6c3de4ad2
Ibrahim <i.aliyu019@gmail.com>
2024-08-10 20:27:48 +0100

    new changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create.user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() payload: CreateUserDto) {
-    return this.usersService.create(payload);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() payload: CreateUserDto) {
-    return this.usersService.update(id, payload);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
-  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,43 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { DatabaseService } from 'src/database/database.service';
-import { CreateUserDto } from './dto/create.user.dto';
 
 @Injectable()
-export class UsersService {
-  constructor(private readonly databaseService: DatabaseService) {}
-  async create(payload: CreateUserDto) {
-    return this.databaseService.user.create({
-      data: payload,
-    });
-  }
-
-  async findAll() {
-    return this.databaseService.user.findMany();
-  }
-
-  async findOne(id: string) {
-    return this.databaseService.user.findUnique({
-      where: {
-        id,
-      },
-    });
-  }
-
-  async update(id: string, payload: Prisma.UserUpdateInput) {
-    return this.databaseService.user.update({
-      where: {
-        id,
-      },
-      data: payload,
-    });
-  }
-
-  async remove(id: string) {
-    return this.databaseService.user.delete({
-      where: {
-        id,
-      },
-    });
-  }
-}
+export class UsersService {}
