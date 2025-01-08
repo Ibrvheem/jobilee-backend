6b7339c452062eaa9e4633f138d512fc9d0ed2e7
ibrahim <i.aliyu019@gmail.com>
2025-01-08 19:08:08 +0100

    refactor: remove user ID parameter from getHello method in AppService and AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,13 @@ import { AppService } from './app.service';
 import { User } from 'decorators/user.decorator';
 import { Public } from 'decorators/public.decorator';
 
+@Public()
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Get()
   async getHello(@User() user): Promise<string> {
-    return await this.appService.getHello(user.id);
+    return await this.appService.getHello();
   }
 }
