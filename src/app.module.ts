7e4baecf71d69fd75db7642a166b42909048724e
ibrahim <i.aliyu019@gmail.com>
2025-01-08 19:44:39 +0100

    refactor: migrate MongoDB connection to use ConfigService for better configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './auth/strategies/jwt.strategy';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from 'guards/jwt.guard';
@@ -13,21 +12,24 @@ import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UdusModule } from './udus/udus.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://dailypilot-dev-admin:$password1@dailypilot-dev.lor8c.mongodb.net/?retryWrites=true&w=majority&appName=DailyPilot-Dev',
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     DatabaseModule,
     AuthModule,
     UsersModule,
     UdusModule,
-
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
     TasksModule,
     // UploadModule,
   ],
