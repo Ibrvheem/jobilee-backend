cc1248279346297a9dacf02379933e63b6086633
ibrahim <i.aliyu019@gmail.com>
2025-04-05 17:10:05 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -11,6 +11,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest-js Swagger Example API')
+    .setDescription('Swagger Example API API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger path: http://localhost:3200/api/docs
+  SwaggerModule.setup(`api/docs`, app, document);
+
   await app.listen(9308);
   console.log('app Listening at port 9308');
 }
